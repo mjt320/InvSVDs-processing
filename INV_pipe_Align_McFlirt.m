40bd727d8e8c95c2b1b97f785b72f340c7ca6c5d
mjt320 <36667519+mjt320@users.noreply.github.com>
2018-03-26 15:37:11 +0100

    Function to align using McFlirt
    (useful to provide motion parameters)

---
+++ INV_pipe_Align_McFlirt.m
@@ -0,0 +1,19 @@
+function INV_pipe_Align_McFlirt(opts)
+
+delete([opts.DCENIIDir '/bet*']); delete([opts.DCENIIDir '/rDCE*']); delete('DCE3D*'); delete([opts.DCENIIDir '/meanPre.nii']);
+
+load([opts.DCENIIDir '/acqPars']);
+
+betRefFile=[opts.DCENIIDir '/betDCE3D0000'];
+refFile=[opts.DCENIIDir '/DCE3D0000'];
+
+system(['mcflirt -in ' opts.DCENIIDir '/DCE -out ' opts.DCENIIDir '/rDCE -plots -mats']);
+system(['fslchfiletype NIFTI ' opts.DCENIIDir '/rDCE']);
+
+SI4DHdr=spm_vol([opts.DCENIIDir '/rDCE.nii']); %load co-reg data to calculate mean pre-contrast image
+[SI4D,temp]=spm_read_vols(SI4DHdr);
+
+meanPreContrast=squeeze(mean(SI4D(:,:,:,1:opts.DCENFramesBase),4)); %calculate average pre-contrast image
+SPMWrite4D(SI4DHdr,meanPreContrast,opts.DCENIIDir,'meanPre',SI4DHdr(1).dt)
+
+end
